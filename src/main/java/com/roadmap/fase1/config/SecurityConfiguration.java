6f650e59a4a2100f86e54f5763fa9feb1b26f820
dudscode <eduardaa9@hotmail.com>
2025-08-30 20:54:21 -0300

    refactor: h2-console SecurityConfiguration

--- src/main/java/com/roadmap/fase1/config/SecurityConfiguration.java
+++ src/main/java/com/roadmap/fase1/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,11 +22,11 @@ public class SecurityConfiguration {
     @Autowired
     SecurityFilter securityFilter;
 
-    private String[] WHITELIST = {
+    private final String[] WHITELIST = {
             "/auth/register",
             "/auth/login"
     };
-    private String[] swaggerWhitelist = {
+    private final String[] swaggerWhitelist = {
             "/v3/api-docs/**",
             "/swagger-ui/**",
             "/swagger-ui.html",
@@ -37,9 +38,11 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequests -> {
                     authorizeRequests.requestMatchers(swaggerWhitelist).permitAll();
+                    authorizeRequests.requestMatchers("/h2-console/**").permitAll();
                     authorizeRequests.requestMatchers(HttpMethod.POST,WHITELIST).permitAll();
                     authorizeRequests.requestMatchers(HttpMethod.GET,"/user/all").hasAnyRole("ADMIN");
                     authorizeRequests.anyRequest().authenticated();
